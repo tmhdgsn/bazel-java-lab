b1a3149b6d28dec326f160deb6b9cb86aaaff817
Tom Hodgson <tmhdgsn@protonmail.com>
2022-07-03 11:43:41 +0100

    init helloworld grpc server

---
+++ helloworld/HelloWorldServer.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+import io.grpc.stub.StreamObserver;
+
+
+public class HelloWorldServer {
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+        @Override
+        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+}
